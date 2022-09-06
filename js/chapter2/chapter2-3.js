79ce23d0a0ded252d32dfbfc4153c9bf6dcf2681
Jack Tsang <cic_tian2@yahoo.com>
2022-09-06 11:13:31 -0700

    Completed solution for 2-3 in Eloquent JavaScript

---
+++ js/chapter2/chapter2-3.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 8; i++) {
+    /* If it's even, print a row starting with a light square */
+    if (i % 2 == 0)
+        console.log(" # # # #");
+    else
+        console.log("# # # # ");
+}
